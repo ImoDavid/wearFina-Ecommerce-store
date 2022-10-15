72505c67af0493d3158da1d795ba72d62d904563
Imo David <emperorjagz@gmail.com>
2022-10-15 17:54:17 +0100

    implemented swiperjs

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,132 +1,120 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import ArrowLeftOutlinedIcon from '@material-ui/icons/ArrowLeftOutlined';
-import ArrowRightOutlinedIcon from '@material-ui/icons/ArrowRightOutlined';
-import { sliderItems } from '../data';
+import { useState } from "react";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
+import { Autoplay, Pagination, } from "swiper";
+import "swiper/swiper-bundle.min.css";
+import { sliderItems } from "../data";
 import { mobile } from "../responsive";
 
 const Container = styled.div`
-    width:100%;
-    height:100vh;
-    display:flex;
-    position:relative;
-    overflow:hidden;
-    ${mobile({ height: "50vh"})}
-    
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+  ${mobile({ height: "50vh" })}
+`;
+const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  transition: all 1.5s ease;
+  ${mobile({ flexDirection: "column" })}
+`;
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(
+    to right bottom,
+    rgb(255, 255, 255) 0%,
+    rgb(255, 255, 255) 70%,
+    ${(props) => props.bg}
+  );
+
+  ${mobile({ height: "50vh" })}
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Image = styled.img`
+  height: 80%;
+
+  ${mobile({
+    width: "100%",
+    objectFit: "contain",
+    display: "flex",
+    justifyContent: "center",
+  })}
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+  text-transform: uppercase;
+  ${mobile({ padding: "20px" })}
+`;
+const Title = styled.h1`
+  font-size: 70px;
+
+  ${mobile({ fontSize: "20px" })}
 `;
 
-const Arrow = styled.div`
-    width:50px;
-    height:50px;
-    background-color:#fff7f7;
-    display:flex;
-    align-items:center;
-    justify-content: center;
-    position:absolute;
-    top:0;
-    bottom:0;
-    left: ${props=> props.direction === "left" && "10px"};
-    right: ${(props)=> props.direction === "right" && "10px"};
-    margin:auto;
-    cursor:pointer;
-    opacity:0.5;
-    border-radius: 50%;
-    z-index: 2;
-    ${mobile({ height: "25px", width:"25px"})}
-`
-const Wrapper = styled.div`
-    display:flex;
-    height:100%;
-    transition:all 1.5s ease;
-    transform:translateX(${(props)=> props.slideIndex * - 100}vw);
-`
-const Slide = styled.div`
-    display:flex;
-    align-items:center;
-    width:100vw;
-    height:100vh;
-    background-color: ${props=> props.bg}; 
-
-    ${mobile({ height: "50vh"})}
-`
-const ImgContainer = styled.div`
-    height:100%;
-    flex:1;
-    display:flex;
-    
-`
-const Image = styled.img`
-height:80%;
- 
-${mobile({width:"100%", objectFit:"cover",display:"flex",justifyContent:"center"})}
-`
-const InfoContainer = styled.div`
-    flex:1;
-    padding:50px;
-    text-transform: uppercase;
-    ${mobile({ padding: "20px"})}
-`
-const Title = styled.h1`
-    font-size:70px;
-
-    ${mobile({ fontSize: "20px"})}
-`
-
 const Desc = styled.p`
-    margin:50px 0px;
-    font-size:20px;
-    font-weight:500;
-    letter-spacing:3px;
-    ${mobile({ fontSize: "12px", margin:"20px 0",})}
-`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+  ${mobile({ fontSize: "12px", margin: "20px 0" })}
+`;
 
 const Button = styled.button`
-    padding:10px;
-    background-color: transparent;
-    font-size:20px;
-    cursor:pointer;
-    ${mobile({ fontSize: "15px",padding:"5px"})}
-`
+  padding: 10px;
+  background-color: transparent;
+  font-size: 20px;
+  cursor: pointer;
+  ${mobile({ fontSize: "15px", padding: "5px" })}
+`;
 
 const Slider = () => {
-    const [slideIndex, setSlideIndex] = useState(0);
-    const handleClick = (direction) => {
-        if (direction === "right"){
-            
-            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
-        }else{
-            
-            setSlideIndex(slideIndex > 2 ? slideIndex + 1 : 0);
-        }
 
-    }
-
-    return ( 
-        <Container> 
-            <Arrow direction="left" onClick={()=> handleClick("left")}>
-                <ArrowLeftOutlinedIcon/>
-            </Arrow>
-            <Wrapper slideIndex = {slideIndex}>
-                {sliderItems.map((item)=>(
-                    <Slide bg={item.bg} key={item.id}>
+  return (
+    <Container>
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        speed={500}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{ clickable: true }}
+        className="mySwiper"
+      >
+        <Wrapper>
+          {sliderItems.map((item) => (
+            <SwiperSlide key={item.id}>
+              <Slide bg={item.bg}>
                 <ImgContainer>
-                    <Image src={item.img}/> 
+                  <Image src={item.img} />
                 </ImgContainer>
                 <InfoContainer>
-                    <Title>{item.title}</Title>
-                    <Desc> {item.desc}</Desc>
-                    <Button> shop now</Button>
+                  <Title>{item.title}</Title>
+                  <Desc> {item.desc}</Desc>
+                  <Button> shop now</Button>
                 </InfoContainer>
-                </Slide>
+              </Slide>
+            </SwiperSlide>
+          ))}
+    
+        </Wrapper>
+      </Swiper>
+    </Container>
+  );
+};
 
-                ))}
-                
-            </Wrapper>
-             <Arrow direction="right" onClick={()=> handleClick("right")}>
-                <ArrowRightOutlinedIcon/>
-            </Arrow>
-        </Container>
-     );
-}
- 
 export default Slider;
--- src/data.js
+++ src/data.js
@@ -1,86 +1,84 @@
- export const sliderItems = [
+export const sliderItems = [
     {
-        id:1,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
-        title:"summer sale",
-        desc:" don't compromise on style! get flat 30% off",
-        bg:"red",
+      id: 1,
+      img: "https://img.freepik.com/free-psd/retro-tv-mockup_1310-1114.jpg?w=740&t=st=1665850784~exp=1665851384~hmac=c6e55ce7bac55970f059825e39899c60fd48047281286e73f9307fb98cdeead1",
+      title: "vintage trade",
+      desc: " don't compromise on style! get flat 30% off",
+      bg: " rgb(191, 139, 191)",
     },
     {
-        id:2,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
-        title:"winter sale",
-        desc:" don't compromise on style! get flat 30% off",
-        bg:"f5fafd",
+      id: 2,
+      img: "https://img.freepik.com/free-photo/young-woman-with-beautiful-slim-perfect-body-bikini-isolated-white-background_186202-3368.jpg?w=360&t=st=1665703693~exp=1665704293~hmac=59f5688592380f3616489805407194872a022287eddd633b4b764efe7e78dfd6",
+      title: "summer sale",
+      desc: " don't compromise on style! get flat 30% off",
+      bg: "f5fafd",
     },
-    {
-        id:3,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
-        title:"sexy sale",
-        desc:" don't compromise on style! get flat 30% off",
-        bg:"f5fafd",
-    }
- ]
+  {
+    id: 3,
+    img: "https://img.freepik.com/free-photo/fashion-shoes-sneakers_1203-7528.jpg?w=740&t=st=1664322341~exp=1664322941~hmac=395b8df41692761d10cd43550e6d651694b0a25b0261e84d3b56129a0409f4a8",
+    title: "winter sale",
+    desc: " don't compromise on style! get flat 30% off",
+    bg: "red",
+  },
+];
 
- export const categories = [
-    {
-        id:1,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
-        title:"SHIRT STYLE",
-        cate:"jewelery"
-    },
-    {
-        id:2,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
-        title:"SHIRT STYLE",
-        cate:"jewelery"
-    },
-    {
-        id:3,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
-        title:"SHIRT STYLE",
-        cate:"jewelery"
-        
-    },
-    {
-        id:4,
-        img:"https://i.ibb.co/XsdmR2c/1.png",
-        title:"SHIRT STYLE",
-        cate:"jewelery"
-        
-    }
- ]
- export const popularproducts = [
-    {
-        id:1,
-        img:"https://img.freepik.com/free-psd/isolated-grey-hoodie-front_125540-1234.jpg?w=740&t=st=1664322047~exp=1664322647~hmac=d36d66bb1f56da214cd7a872d8c8260de7a71d62c8407bfabc1d7018e8db213d",
-    },
-    {
-        id:2,
-        img:"https://img.freepik.com/free-psd/isolated-blue-t-shirt_125540-1229.jpg?w=740&t=st=1664322233~exp=1664322833~hmac=07ca5c97347dad989cb315c8a8569f11d1ceb49da4d483b6a9cd778f5c9d4816",
-    },
-    {
-        id:3,
-        img:"https://img.freepik.com/free-photo/fashion-shoes-sneakers_1203-7528.jpg?w=740&t=st=1664322341~exp=1664322941~hmac=395b8df41692761d10cd43550e6d651694b0a25b0261e84d3b56129a0409f4a8",
-    },
-    {
-        id:4,
-        img:"https://img.freepik.com/premium-psd/6318-blue-denim-jeans-isolated-transparent-background_176765-1076.jpg?w=740",
-    },
-    {
-        id:5,
-        img:"https://pngimg.com/uploads/dress/dress_PNG183.png://placehold.jp/3d4070/ffffff/150x150.png",
-    },
-    {
-        id:6,
-        img:"https://placeimg.com/300/303/arch",
-    },
-    {
-        id:7,
-        img:"https://placehold.jp/3d4070/ffffff/150x150.png",
-    },
-    {
-        id:8,
-        img:"https://placehold.jp/3d4070/ffffff/150x150.png",
-    },
- ]
+export const categories = [
+  {
+    id: 1,
+    img: "https://placeimg.com/300/303/any",
+    title: "SHIRT STYLE",
+    cate: "jewelery",
+  },
+  {
+    id: 2,
+    img: "https://placeimg.com/300/303/any",
+    title: "SHIRT STYLE",
+    cate: "jewelery",
+  },
+  {
+    id: 3,
+    img: "https://placeimg.com/300/303/any",
+    title: "SHIRT STYLE",
+    cate: "jewelery",
+  },
+  {
+    id: 4,
+    img: "https://placeimg.com/300/303/any",
+    title: "SHIRT STYLE",
+    cate: "jewelery",
+  },
+];
+export const popularproducts = [
+  {
+    id: 1,
+    img: "https://img.freepik.com/free-psd/isolated-grey-hoodie-front_125540-1234.jpg?w=740&t=st=1664322047~exp=1664322647~hmac=d36d66bb1f56da214cd7a872d8c8260de7a71d62c8407bfabc1d7018e8db213d",
+  },
+  {
+    id: 2,
+    img: "https://img.freepik.com/free-psd/isolated-blue-t-shirt_125540-1229.jpg?w=740&t=st=1664322233~exp=1664322833~hmac=07ca5c97347dad989cb315c8a8569f11d1ceb49da4d483b6a9cd778f5c9d4816",
+  },
+  {
+    id: 3,
+    img: "https://img.freepik.com/free-photo/fashion-shoes-sneakers_1203-7528.jpg?w=740&t=st=1664322341~exp=1664322941~hmac=395b8df41692761d10cd43550e6d651694b0a25b0261e84d3b56129a0409f4a8",
+  },
+  {
+    id: 4,
+    img: "https://img.freepik.com/premium-psd/6318-blue-denim-jeans-isolated-transparent-background_176765-1076.jpg?w=740",
+  },
+  {
+    id: 5,
+    img: "https://pngimg.com/uploads/dress/dress_PNG183.png://placehold.jp/3d4070/ffffff/150x150.png",
+  },
+  {
+    id: 6,
+    img: "https://placeimg.com/300/303/arch",
+  },
+  {
+    id: 7,
+    img: "https://placehold.jp/3d4070/ffffff/150x150.png",
+  },
+  {
+    id: 8,
+    img: "https://placehold.jp/3d4070/ffffff/150x150.png",
+  },
+];
